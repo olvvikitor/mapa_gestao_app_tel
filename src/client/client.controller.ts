de6b499997d033cea362343194faebf526f483ce
olvvikitor <victoroliveira.3002@gmail.com>
2025-01-27 14:39:49 -0300

    --add css for indexs

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -2,15 +2,16 @@ import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { join } from 'path';
 
-@Controller('mapa')
+@Controller()
 export class ClientController {
   @Get(':page')
   async servePage(@Res() res: Response, @Param('page') page: string) {
     // Caminho para o diretório específico do cliente
     const clientPath = join(__dirname,'..','..', 'src', 'client', page);
 
-    
+
     // Se a pasta existir, serve o index.html dessa pasta
+    //exemplo o index.html de login...
     try {
       return res.sendFile(join(clientPath, 'index.html'));
     } catch (error) {
